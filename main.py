7196e34d0333d69ee6e9068b413dddb753555f48
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-11-19 06:06:22 +0500

    worker

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
         workers.append(worker_process)
 
     try:
-        uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+        uvicorn.run(app, host="0.0.0.0", port=8000)
     finally:
         for _ in workers:
             audio_queue.put(None)
